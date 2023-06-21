044039c3a4349fae7ee62f2f37a4f4962571a162
Anderson Schmidt <anderson.schmidt90@gmail.com>
2023-06-21 17:36:56 -0300

    comentado o que cada comando faz no arquivo cons_preco

--- pages/controle/cons_preco.php
+++ pages/controle/cons_preco.php
@@ -1,25 +1,31 @@
 <?php
 session_start();
 
+// Inclui o arquivo de configuração do banco de dados
 require_once('../database/database.php');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['remedio'])) {
+    // Obtém o valor do campo de formulário 'remedio'
     $desc = $_POST['remedio'];
 
+    // Monta a consulta SQL para selecionar os registros da tabela 'remedios'
     $sql = "SELECT produto, pd_sem_impostos AS remedio FROM remedios";
 
     if (!empty($desc)) {
+        // Adiciona uma cláusula WHERE à consulta se o campo 'remedio' não estiver vazio
         $sql .= " WHERE produto = '" . $desc . "'";
     }
 
+    // Executa a consulta no banco de dados
     $res = $con->query($sql);
 
     // Aqui você pode processar o resultado da consulta e exibir os dados na página
     if ($res) {
-       $remedios = $res->fetchAll(PDO::FETCH_ASSOC);
+        // Obtém todos os resultados da consulta como um array associativo
+        $remedios = $res->fetchAll(PDO::FETCH_ASSOC);
     } else {
+        // Define um array vazio se a consulta não retornar resultados
         $remedios = [];
     }
 }
 ?>
-
