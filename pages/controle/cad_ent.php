7974acc79e73afca2c7c715ceced9e51bd41f845
Anderson Schmidt <anderson.schmidt90@gmail.com>
2023-06-21 17:35:58 -0300

    comentado o que cada comando faz no arquivo cad_ent

--- pages/controle/cad_ent.php
+++ pages/controle/cad_ent.php
@@ -4,7 +4,8 @@
     $_SESSION['erro_msg'] = "";
     #error_reporting(E_ERROR | E_PARSE);
     $mysqli = null; 
-   
+
+    // Recupera os valores dos campos de formulário
     $reme = $_POST['remedio'];
     $lot = $_POST['lote'];
     $dt_venc = $_POST['dt_venc'];
@@ -14,62 +15,71 @@
     $id_ctrl  = 0;
 
     try {
+        // Estabelece uma conexão com o banco de dados
         $mysqli = new mysqli("banco", "user", "user", "controlefacil");
+
+        // Prepara uma consulta para selecionar registros na tabela 'medicamento_controle'
         $stm = $mysqli->prepare("select * from medicamento_controle
                                  where id_med = ? and lote = ?; ");
-        $stm->bind_param('is',$reme, $lot );
+        // Faz a ligação dos parâmetros da consulta preparada
+        $stm->bind_param('is', $reme, $lot);
+        // Executa a consulta preparada
         $stm->execute();
+        // Obtém o resultado da consulta e o armazena em um array associativo
         $r = $stm->get_result()->fetch_assoc();
-        if ($r){
-            if($_POST['id_controle'] == "" ) {
-                $qtdRes += $r['qtd'];
+        if ($r) {
+            if ($_POST['id_controle'] == "") {
+                $qtdRes += $r['qtd']; // Adiciona a quantidade existente do estoque
             }
-           $id_ctrl  = $r['id'];
+            $id_ctrl  = $r['id'];
         }
     } catch (\Throwable $th) {
         $_SESSION['erro_msg'] = $th->getMessage();
         #include('../entrada.php'); 
         die;
     } finally {
+        // Fecha a conexão com o banco de dados
         $mysqli->close();
-
     }
 
     try {
+        // Estabelece uma nova conexão com o banco de dados
         $mysqli = new mysqli("banco", "user", "user", "controlefacil");
+        // Inicia uma transação
         $mysqli->begin_transaction();
         
         if ($qtd != $qtdRes) {
+            // Atualiza o registro na tabela 'medicamento_controle' se a quantidade informada for diferente da quantidade existente
             $stm = $mysqli->prepare("update medicamento_controle set dt_vencimento = ?,
                                             lote = ?,
                                             qtd = ?
                                     where id = ?;");
-            $stm->bind_param('ssii',$dt_venc,$lot,$qtdRes, $id_ctrl);
+            $stm->bind_param('ssii', $dt_venc, $lot, $qtdRes, $id_ctrl);
             $stm->execute();           
         } else {
-            $stm = $mysqli->prepare("insert into medicamento_controle(dt_vencimento,lote,qtd,id_med) values (?,?,?,?);");
-            $stm->bind_param('ssii',$dt_venc,$lot,$qtdRes, $reme);
+            // Insere um novo registro na tabela 'medicamento_controle' se a quantidade informada for igual à quantidade existente
+            $stm = $mysqli->prepare("insert into medicamento_controle(dt_vencimento, lote, qtd, id_med) values (?,?,?,?);");
+            $stm->bind_param('ssii', $dt_venc, $lot, $qtdRes, $reme);
             $stm->execute();
+            // Obtém o ID do registro recém-inserido
             $id_ctrl = $mysqli->insert_id;
         }
         
-        $stm = $mysqli->prepare("insert into bordero(dt_evento,	
-                                                    id_med_ctrl,
-                                                    qtd)
-                                VALUES(?,?,?)");
-                                
-        $stm->bind_param('sii',$dt_evento, $id_ctrl, $qtd);
+        // Insere um novo registro na tabela 'bordero'
+        $stm = $mysqli->prepare("insert into bordero(dt_evento, id_med_ctrl, qtd) VALUES(?,?,?)");
+        $stm->bind_param('sii', $dt_evento, $id_ctrl, $qtd);
         $stm->execute();
+        // Confirma a transação
         $mysqli->commit();
         $_POST['remedio'] = null;
         include('../consulta.php'); 
     } catch (\Throwable $th) {
         $_SESSION['erro_msg'] = $id_ctrl . $th->getMessage();
+        // Desfaz a transação em caso de exceção
         $mysqli->rollback();
         include('../entrada.php'); 
     } finally {
+        // Fecha a conexão com o banco de dados
         $mysqli->close();        
     }
-            
-    
 ?>
