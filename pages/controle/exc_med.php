25c1ee3c1260d4d53f778907323f15d4cc178ee7
Anderson Schmidt <anderson.schmidt90@gmail.com>
2023-06-21 17:39:46 -0300

    comentado o que cada código faz no arquivo exc_med

--- pages/controle/exc_med.php
+++ pages/controle/exc_med.php
@@ -7,33 +7,39 @@ $_SESSION['alert_msg'] = "";
 #error_reporting(E_ERROR | E_PARSE);
 $mysqli = null;
 
-
 $var_id = $_GET['id_med'];
 
 try {
+    // Constrói a consulta SQL para selecionar o ID na tabela 'medicamento_controle'
     $sql = "select id from medicamento_controle where id = ".$var_id;
+    
+    // Executa a consulta e obtém todos os resultados
     $res = $con->query($sql)->fetchAll();
-  /*  $res->bind_param('i',$var_id);
-    $res->execute();
-    $res->bind_result($id);*/
-   // print_r($res);
-   foreach ($res as $obj) {
-
+    
+    // Itera sobre os resultados retornados
+    foreach ($res as $obj) {
+        // Constrói a consulta SQL para excluir o registro com o ID específico
         $sql = "delete from medicamento_controle where id = :id";
+        
+        // Prepara a consulta
         $res = $con->prepare($sql);
-        $res->bindParam(':id',$obj['id']);
+        
+        // Vincula o parâmetro ID à consulta preparada
+        $res->bindParam(':id', $obj['id']);
+        
+        // Executa a consulta preparada para excluir o registro
         $res->execute();
+        
+        // Define uma mensagem de alerta de sucesso na variável de sessão
         $_SESSION['alert_msg'] = "Sucesso!";
     }
-
 } catch (Exception $th) {
+    // Em caso de exceção, define a mensagem de erro na variável de sessão e exibe o erro
     $_SESSION['erro_msg'] = $th->getMessage();
     echo $th->getMessage();
     die;
 } finally {
+    // Inclui o arquivo '../consulta.php'
     include('../consulta.php');
 }
-
-
-
 ?>
