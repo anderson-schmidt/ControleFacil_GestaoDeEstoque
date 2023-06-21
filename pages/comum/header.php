3bb2652efa22149811399491ae6f8ec5f1752510
Anderson Schmidt <anderson.schmidt90@gmail.com>
2023-06-21 17:41:07 -0300

    comentado o que cada código faz no arquivo header.php

--- pages/comum/header.php
+++ pages/comum/header.php
@@ -1,16 +1,22 @@
 <header>
     <?php
+    // Verifica se há uma mensagem de erro na variável de sessão 'erro_msg'
+    // Se houver, exibe uma div de alerta de perigo com a mensagem de erro
     if ($_SESSION['erro_msg'] != '') {
         echo '<div class="alert alert-danger" role="alert">';
         echo $_SESSION['erro_msg'];
         echo '</div>';
-    } ?>
+    }
+    ?>
     <?php
+    // Verifica se há uma mensagem de alerta na variável de sessão 'alert_msg'
+    // Se houver, exibe uma div de alerta de sucesso com a mensagem de alerta
     if ($_SESSION['alert_msg'] != '') {
         echo '<div class="alert alert-success" role="alert">';
         echo $_SESSION['alert_msg'];
         echo '</div>';
-    } ?>
+    }
+    ?>
     <div class="boasVindas">
         <div class="bv">
             Bem vindo
