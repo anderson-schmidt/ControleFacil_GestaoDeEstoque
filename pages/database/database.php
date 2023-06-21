af78a0cf73bb181c15e4600cebc7bb0d851042a6
Anderson Schmidt <anderson.schmidt90@gmail.com>
2023-06-21 17:42:47 -0300

    Comentado o que os comandos fazem no arquivo database

--- pages/database/database.php
+++ pages/database/database.php
@@ -1,13 +1,21 @@
 <?php
 try {
+    // Variável global para armazenar a conexão com o banco de dados
     global $con;
- $con = new PDO("mysql:host=banco;dbname=controlefacil", "user", "user");
- $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    // Criação de uma nova instância da classe PDO para estabelecer a conexão
+    // A string de conexão especifica o host como "banco", o nome do banco de dados como "controlefacil",
+    // e o nome de usuário e senha como "user"
+    $con = new PDO("mysql:host=banco;dbname=controlefacil", "user", "user");
+
+    // Definindo o modo de erro para lançar exceções em caso de erros
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e){
+    // Em caso de exceção durante a conexão, imprime o objeto de exceção para depuração
     print_r($e);
+    // Exibe uma mensagem de erro indicando que houve um problema na conexão
     echo 'Erro conexão';
+    // Encerra a execução do programa
     die();
 }
-
 ?>
